754cf2e23c9e20dc9ef2dac1fa19bb775a8efedf
Favasabdulnassr <141939797+Favasabdulnassr@users.noreply.github.com>
2023-08-24 15:01:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function hide() {
+    var x = document.getElementById("hide");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
